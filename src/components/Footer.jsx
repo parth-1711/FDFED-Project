5511ff9ca22ee4af683b2e7b1a6afe2048525143
Digvijay <digvijayghule2001@gmail.com>
2023-11-15 23:38:13 +0530

    Added checkout page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2023 Your Company Name. All rights reserved.</p>
+        <p>&copy; 2023 PHOENIX E-MART. All rights reserved.</p>
       </div>
     </footer>
   );
