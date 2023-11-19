2fd4da65a473033440a4ca46be2ca9feded3bc18
Sandeepkumar1121 <109285726+Sandeepkumar1121@users.noreply.github.com>
2023-11-19 22:11:36 +0530

    MyOffers,sell,landing,help,error(pages)and req css

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import { FiArrowRight } from "react-icons/fi";
+
+
+const Landing = () => {
+  return (
+    <div className="home-container" style={{width:'100vw'}}>
+  
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            We are here to mediate your buyings and sellings
+          </h1>
+          <p className="primary-text">
+            lorem ipsum dolor sit amet, consectetur adip , lorem
+          </p>
+          <button className="secondary-button">
+            Login <FiArrowRight />{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
